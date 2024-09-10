1def4f9c937cbd4e58c7f983c8eb2f60f40fb2f6
Thomas Vergne <contact@thomas-vergne.fr>
2024-09-10 13:56:41 +0200

    feat(ffi): added some new functions about type conversion

--- library/combinator-ffi.js
+++ library/combinator-ffi.js
@@ -8,6 +8,25 @@ module.exports = {
   is_whitespace: (c) =>
     /\s/.test(c),
   ffi_to_int: (s) => parseInt(s),
+  ffi_to_float: (s) => parseFloat(s),
+  str_to_int(s) {
+    const res = parseInt(s);
+
+    if (isNaN(res)) {
+      return [null, 'Option', 'None'];
+    }
+
+    return [null, 'Option', 'Some', res];
+  },
+  str_to_float(s) {
+    const res = parseFloat(s);
+
+    if (isNaN(res)) {
+      return [null, 'Option', 'None'];
+    }
+
+    return [null, 'Option', 'Some', res];
+  },
   str_split: (s, sep) => s.split(sep),
   which: (s) => {
     const fs = require('fs');
